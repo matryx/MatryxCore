dc5199606d0d871ed176aaee35828b1a88c4b611
Astrovicis <tmaxhoward@gmail.com>
2017-12-12 11:57:57 -0800

    Update init_and_launch_console.bat

--- local-node-windows/init_and_launch_console.bat
+++ local-node-windows/init_and_launch_console.bat
@@ -1,2 +1,2 @@
-Geth.exe --identity "MatryxTestNode" init --datadir C:\Users\BCash\Downloads\Matryx-Alpha-master\Matryx-Alpha-master\local-node-windows "MatryxGenesis.json" --ipcdisable --mine --autodag --networkid 628799 --nat "any" --gasprice "3000000"
-Geth.exe --identity "MatryxTestNode" --datadir C:\Users\BCash\Downloads\Matryx-Alpha-master\Matryx-Alpha-master\local-node-windows console
+Geth.exe --identity "MatryxTestNode" init --datadir %cd% "MatryxGenesis.json" --ipcdisable --mine --autodag --networkid 628799 --nat "any" --gasprice "3000000"
+Geth.exe --identity "MatryxTestNode" --datadir %cd% console
