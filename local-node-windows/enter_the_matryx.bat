ba4cd442618d539c154af55cde822c5f075ebdb8
Astrovicis <tmaxhoward@gmail.com>
2017-12-12 11:44:52 -0800

    Separated scripts to make importing an account easier.

--- local-node-windows/enter_the_matryx.bat
+++ local-node-windows/enter_the_matryx.bat
@@ -1,10 +1,4 @@
-Geth.exe ^
-    --identity "MatryxTestNode" ^
-    init ^
-    --datadir %cd% ^
-    "MatryxGenesis.json" ^
-    --ipcdisable ^
-    --mine --autodag --networkid 628799 --nat "any" --gasprice "3000000"
+
 
 Geth.exe ^
     --identity "MatryxTestNode" ^
