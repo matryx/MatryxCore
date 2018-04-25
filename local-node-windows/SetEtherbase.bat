48d0544ea03144108b92b6c282dcf5332b3056eb
Astrovicis <tmaxhoward@gmail.com>
2018-04-25 08:30:50 -0700

    Update SetEtherbase.bat for paths with spaces

--- local-node-windows/SetEtherbase.bat
+++ local-node-windows/SetEtherbase.bat
@@ -1,2 +1,2 @@
-Geth.exe --identity "MatryxTestNode" init --datadir %cd% "MatryxGenesis.json" --ipcdisable --mine --autodag --networkid 628799 --nat "any" --gasprice "3000000"
-Geth.exe --identity "MatryxTestNode" --datadir %cd% console
+Geth.exe --identity "MatryxTestNode" init --datadir "%cd%" "MatryxGenesis.json" --ipcdisable --mine --autodag --networkid 628799 --nat "any" --gasprice "3000000"
+Geth.exe --identity "MatryxTestNode" --datadir "%cd%" console
